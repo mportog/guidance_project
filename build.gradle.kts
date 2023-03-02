8c7ecf68011f0564c8ebb014af730e0cff2da79c
Matheus Porto <matheus.porto@picpay.com>
2023-03-02 09:41:22 -0300

    adiciona ktlint parra rodar local tb

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,17 @@ plugins {
     id("com.android.application") version "7.1.3" apply false
     id("com.android.library") version "7.1.3" apply false
     id("org.jetbrains.kotlin.android") version "1.6.21" apply false
+    id("org.jlleitschuh.gradle.ktlint") version "11.2.0"
+}
+
+ktlint {
+    configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
+        debug.set(true)
+        android.set(true)
+        ignoreFailures.set(false)
+        outputToConsole.set(true)
+        outputColorName.set("RED")
+    }
 }
 
 tasks {
